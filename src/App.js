09c9fce127d5164f4f7098143316ca7178db8ae8
pavelsch00 <pavelsch00@gmail.com>
2022-05-02 20:57:57 +0300

    Test ui with api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent  from './components/MyComponent';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Test
         </a>
+        <MyComponent/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/MyComponent.js
@@ -0,0 +1,46 @@
+import {useState, useEffect} from 'react';
+
+function MyComponent() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    // Примечание: пустой массив зависимостей [] означает, что
+    // этот useEffect будет запущен один раз
+    // аналогично componentDidMount()
+    useEffect(() => {
+      fetch('https://inventory-control-of-dep.herokuapp.com/WeatherForecast')
+        .then(
+          res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+          // чтобы не перехватывать исключения из ошибок в самих компонентах.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.date} {item.temperatureC} {item.temperatureF} {item.summary}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+  export default MyComponent;
