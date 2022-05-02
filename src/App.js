882bfad20340e155274b54861788243f4a398599
pavelsch00 <pavelsch00@gmail.com>
2022-05-02 19:56:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <link>https://inventory-control-of-dep.herokuapp.com/WeatherForecast</link>
         <a
           className="App-link"
           href="https://reactjs.org"
